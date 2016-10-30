e399fb0412838fb0af78645310cf9e3db1956886
Dylan Piercey <pierceydylan@gmail.com>
2016-10-30 10:36:15 -0700

    Fix unsafe eval

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ global.SECURITY = !IS_DEV && {
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", '"unsafe-eval"', 'www.google-analytics.com'],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'www.google-analytics.com'],
       styleSrc: ["'self'", "'unsafe-inline'"],
       fontSrc: ["'self'"],
       imgSrc: ["'self'", 'data:', 'rill.site', 'img.shields.io', 'badge-size.herokuapp.com', 'camo.githubusercontent.com', 'www.google-analytics.com', 'stats.g.doubleclick.net'],
